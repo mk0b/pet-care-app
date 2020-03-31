2759b80ee3be65d4e64b33af177506ad830ebca5
Megan Katherine O'Brien <megankob@gmail.com>
2020-03-31 14:37:45 -0400

    Starting setup for Mongoose Models

---
+++ api/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+//create a schema | describing how the data should look
+const UserSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    emailAddress: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: Date.now
+});
+
+//below creates the model gives it a name and tells it which schema to use for mlabs
+module.exports = mongoose.model('Users', UserSchema);
